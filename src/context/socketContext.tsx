f3d4b11b093d9da6aedb762ece8811dbe1cfbfb2
Tonalli López <aguilablanca_7@live.com>
2021-10-18 10:03:22 -0500

    🩹 Move protocol websocket connection
    Move protocol websocket connection to .env file

--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -13,7 +13,7 @@ export const SocketContextProvider: FC<Props> = ({ children }) => {
   const [socket, setSocket] = useState<Socket|null>(null);
 
   useEffect(() => {
-    const socketio = io(`ws://${process.env.NEXT_PUBLIC_HOSTSOCKET}/`, {
+    const socketio = io(`${process.env.NEXT_PUBLIC_HOSTSOCKET}`, {
       transports: ["websocket", "polling"] // use WebSocket first, if available
     });
 
